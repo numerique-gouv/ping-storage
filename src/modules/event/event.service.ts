28f0c5a36305184a090b227f2b94c4fdf5cba95f
Benoit Serrano <benoit.serrano10@gmail.com>
2024-12-06 17:44:41 +0100

    move aggregate events function

--- src/modules/event/event.service.ts
+++ src/modules/event/event.service.ts
@@ -1,8 +1,12 @@
 import { dataSource } from '../../dataSource';
+import { generateArray } from '../../lib/utils';
 import { SystemPulse } from '../systemPulse';
 import { Event } from './Event.entity';
 
 export { buildEventService };
+
+type rangeType = { timestamp: number; upPercentage: number | undefined };
+type rangePeriod = 'day' | 'hours';
 
 function buildEventService() {
     const eventRepository = dataSource.getRepository(Event);
@@ -12,6 +16,7 @@ function buildEventService() {
         getLastEvent,
         getEventsForSystemPulse,
         getAllEvents,
+        aggregateEvents,
     };
 
     return eventService;
@@ -48,5 +53,113 @@ function buildEventService() {
             kind: params.kind,
         });
         return true;
+    }
+
+    function aggregateEvents(
+        events: Event[],
+        period: rangePeriod,
+        periodCount: number,
+        now: number,
+    ): rangeType[] {
+        const PERIOD_IN_MILLISECONDS = computePeriodInMilliseconds(period);
+        const ranges: rangeType[] = generateArray(periodCount).map((_, index) => ({
+            upPercentage: undefined,
+            timestamp: now - (periodCount - index) * PERIOD_IN_MILLISECONDS,
+        }));
+        if (events.length === 0) {
+            return ranges;
+        }
+        const firstEventTimestamp = computeTimestamp(events[0]);
+        for (let rangeIndex = 0; rangeIndex < ranges.length; rangeIndex++) {
+            if (firstEventTimestamp < ranges[rangeIndex].timestamp) {
+                ranges[rangeIndex].upPercentage = 0;
+            }
+        }
+
+        for (let eventIndex = 0; eventIndex < events.length; eventIndex++) {
+            for (let rangeIndex = 0; rangeIndex < periodCount; rangeIndex++) {
+                const currentEvent = events[eventIndex];
+                if (currentEvent.kind === 'up') {
+                    const currentEventTimestamp = computeTimestamp(currentEvent);
+                    let nextEventTimestamp =
+                        eventIndex < events.length - 1
+                            ? computeTimestamp(events[eventIndex + 1])
+                            : undefined;
+                    const currentRangeLeftTimestamp = ranges[rangeIndex].timestamp;
+                    const currentRangeRightTimestamp =
+                        ranges[rangeIndex].timestamp + PERIOD_IN_MILLISECONDS;
+
+                    if (currentEventTimestamp >= currentRangeRightTimestamp) {
+                        continue;
+                    }
+                    if (
+                        nextEventTimestamp !== undefined &&
+                        nextEventTimestamp < currentRangeLeftTimestamp
+                    ) {
+                        continue;
+                    }
+                    if (currentEventTimestamp <= currentRangeLeftTimestamp) {
+                        if (
+                            nextEventTimestamp === undefined ||
+                            nextEventTimestamp >= currentRangeRightTimestamp
+                        ) {
+                            ranges[rangeIndex].upPercentage = 100;
+                        }
+
+                        if (
+                            nextEventTimestamp !== undefined &&
+                            nextEventTimestamp < currentRangeRightTimestamp
+                        ) {
+                            const addedPercentage =
+                                (100 * (nextEventTimestamp - currentRangeLeftTimestamp)) /
+                                PERIOD_IN_MILLISECONDS;
+
+                            ranges[rangeIndex].upPercentage =
+                                (ranges[rangeIndex].upPercentage || 0) + addedPercentage;
+                        }
+                    }
+
+                    if (currentEventTimestamp > currentRangeLeftTimestamp) {
+                        if (
+                            nextEventTimestamp === undefined ||
+                            nextEventTimestamp >= currentRangeRightTimestamp
+                        ) {
+                            const addedPercentage =
+                                (100 * (currentRangeRightTimestamp - currentEventTimestamp)) /
+                                PERIOD_IN_MILLISECONDS;
+                            ranges[rangeIndex].upPercentage =
+                                (ranges[rangeIndex].upPercentage || 0) + addedPercentage;
+                        }
+
+                        if (
+                            nextEventTimestamp !== undefined &&
+                            nextEventTimestamp < currentRangeRightTimestamp
+                        ) {
+                            const addedPercentage =
+                                (100 * (nextEventTimestamp - currentEventTimestamp)) /
+                                PERIOD_IN_MILLISECONDS;
+
+                            ranges[rangeIndex].upPercentage =
+                                (ranges[rangeIndex].upPercentage || 0) + addedPercentage;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ranges;
+    }
+}
+
+function computeTimestamp(event: Event) {
+    return new Date(event.createdAt).getTime();
+}
+
+function computePeriodInMilliseconds(period: rangePeriod): number {
+    switch (period) {
+        case 'hours':
+            return 60 * 60 * 1000;
+        case 'day':
+            return 24 * 60 * 60 * 1000;
     }
 }
--- src/modules/systemPulse/systemPulse.service.ts
+++ src/modules/systemPulse/systemPulse.service.ts
@@ -1,13 +1,9 @@
 import { dataSource } from '../../dataSource';
-import { generateArray } from '../../lib/utils';
-import { buildEventService, Event } from '../event';
+import { buildEventService } from '../event';
 import { eventKindType } from '../event/types';
 import { SystemPulse } from './SystemPulse.entity';
 
 export { buildSystemPulseService };
-
-type rangeType = { timestamp: number; upPercentage: number | undefined };
-type rangePeriod = 'day' | 'hours';
 
 function buildSystemPulseService() {
     const systemPulseRepository = dataSource.getRepository(SystemPulse);
@@ -19,7 +15,6 @@ function buildSystemPulseService() {
         getSystemPulseSummary,
         pingSystemPulse,
         checkAllSystemPulses,
-        aggregateEvents,
     };
 
     return systemPulseService;
@@ -134,122 +129,5 @@ function buildSystemPulseService() {
             status,
             events,
         };
-    }
-
-    function aggregateEvents(
-        events: Event[],
-        period: rangePeriod,
-        periodCount: number,
-        now: number,
-    ): rangeType[] {
-        const PERIOD_IN_MILLISECONDS = computePeriodInMilliseconds(period);
-        const ranges: rangeType[] = generateArray(periodCount).map((_, index) => ({
-            upPercentage: undefined,
-            timestamp: now - (periodCount - index) * PERIOD_IN_MILLISECONDS,
-        }));
-        if (events.length === 0) {
-            return ranges;
-        }
-        const firstEventTimestamp = computeTimestamp(events[0]);
-        for (let rangeIndex = 0; rangeIndex < ranges.length; rangeIndex++) {
-            if (firstEventTimestamp < ranges[rangeIndex].timestamp) {
-                ranges[rangeIndex].upPercentage = 0;
-            }
-        }
-
-        for (let eventIndex = 0; eventIndex < events.length; eventIndex++) {
-            for (let rangeIndex = 0; rangeIndex < periodCount; rangeIndex++) {
-                const currentEvent = events[eventIndex];
-                if (currentEvent.kind === 'up') {
-                    const currentEventTimestamp = computeTimestamp(currentEvent);
-                    let nextEventTimestamp =
-                        eventIndex < events.length - 1
-                            ? computeTimestamp(events[eventIndex + 1])
-                            : undefined;
-                    const currentRangeLeftTimestamp = ranges[rangeIndex].timestamp;
-                    const currentRangeRightTimestamp =
-                        ranges[rangeIndex].timestamp + PERIOD_IN_MILLISECONDS;
-
-                    if (currentEventTimestamp >= currentRangeRightTimestamp) {
-                        continue;
-                    }
-                    if (
-                        nextEventTimestamp !== undefined &&
-                        nextEventTimestamp < currentRangeLeftTimestamp
-                    ) {
-                        continue;
-                    }
-                    if (currentEventTimestamp <= currentRangeLeftTimestamp) {
-                        if (
-                            nextEventTimestamp === undefined ||
-                            nextEventTimestamp >= currentRangeRightTimestamp
-                        ) {
-                            ranges[rangeIndex].upPercentage = 100;
-                        }
-
-                        if (
-                            nextEventTimestamp !== undefined &&
-                            nextEventTimestamp < currentRangeRightTimestamp
-                        ) {
-                            const addedPercentage =
-                                (100 * (nextEventTimestamp - currentRangeLeftTimestamp)) /
-                                PERIOD_IN_MILLISECONDS;
-                            if (addedPercentage === -20) {
-                                console.log('currentEventTimestamp', currentEventTimestamp);
-                                console.log('nextEventTimestamp', nextEventTimestamp);
-                                console.log('currentRangeLeftTimestamp', currentRangeLeftTimestamp);
-                                console.log(
-                                    'currentRangeRightTimestamp',
-                                    currentRangeRightTimestamp,
-                                );
-                            }
-
-                            ranges[rangeIndex].upPercentage =
-                                (ranges[rangeIndex].upPercentage || 0) + addedPercentage;
-                        }
-                    }
-
-                    if (currentEventTimestamp > currentRangeLeftTimestamp) {
-                        if (
-                            nextEventTimestamp === undefined ||
-                            nextEventTimestamp >= currentRangeRightTimestamp
-                        ) {
-                            const addedPercentage =
-                                (100 * (currentRangeRightTimestamp - currentEventTimestamp)) /
-                                PERIOD_IN_MILLISECONDS;
-                            ranges[rangeIndex].upPercentage =
-                                (ranges[rangeIndex].upPercentage || 0) + addedPercentage;
-                        }
-
-                        if (
-                            nextEventTimestamp !== undefined &&
-                            nextEventTimestamp < currentRangeRightTimestamp
-                        ) {
-                            const addedPercentage =
-                                (100 * (nextEventTimestamp - currentEventTimestamp)) /
-                                PERIOD_IN_MILLISECONDS;
-
-                            ranges[rangeIndex].upPercentage =
-                                (ranges[rangeIndex].upPercentage || 0) + addedPercentage;
-                        }
-                    }
-                }
-            }
-        }
-
-        return ranges;
-    }
-}
-
-function computeTimestamp(event: Event) {
-    return new Date(event.createdAt).getTime();
-}
-
-function computePeriodInMilliseconds(period: rangePeriod): number {
-    switch (period) {
-        case 'hours':
-            return 60 * 60 * 1000;
-        case 'day':
-            return 24 * 60 * 60 * 1000;
     }
 }
